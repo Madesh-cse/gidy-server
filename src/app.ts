b57b627124ba9e2d31559e98e7d6ea4c9da9ee9c
Madesh <madeshdex10@gmail.com>
2026-02-16 07:50:27 +0530

    update teh cors

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,34 @@
-import {config} from "dotenv"
-import cors from "cors"
+import { config } from "dotenv";
+import cors from "cors";
 
-import express from "express"
+import express from "express";
 import appRouter from "./routes";
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://gidy-client.vercel.app"
-  ],
-  credentials: true
-}));
-app.use(express.json());
-app.use("/uploads", express.static("uploads"))
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://gidy-client.vercel.app",
+];
 
-app.use("/userProfile", appRouter)
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.use("/userProfile", appRouter);
 
 export default app;
